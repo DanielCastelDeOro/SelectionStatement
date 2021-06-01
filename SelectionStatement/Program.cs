53576d03fbfa1cbb59b91e950c20be866a2fec96
Daniel Castel De Oro <daniel.casteldeoro@gmail.com>
2021-06-01 05:13:23 -0600

    Add project files.

---
+++ SelectionStatement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SelectionStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 9;
+
+
+        }
+    }
+}
