bead759b049a8f074f7a5a315a3cd11c657d96c3
Daniel Castel De Oro <daniel.casteldeoro@gmail.com>
2021-06-01 05:15:06 -0600

    commit1 branch1

--- SelectionStatement/Program.cs
+++ SelectionStatement/Program.cs
@@ -8,6 +8,26 @@ namespace SelectionStatement
         {
             int favoriteNumber = 9;
 
+            Console.WriteLine($"Pick your favorite number. Hint: Its below 10.");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine($"Too Low guess again.");
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine($"Nope, too high. TRY AGAIN..");
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("Got it. Good Job!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
 
         }
     }
